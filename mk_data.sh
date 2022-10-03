f43d1f14e875c957b74cac08eb15eb0b00297cb7
Brae Troutman <troupb18@gemini.deac.wfu.edu>
2022-10-03 18:21:26 -0400

    change from xargs to for-loop

--- mk_data.sh
+++ mk_data.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
-echo "length,bc,mergesort,stl_sort,speedup" > data/problem_three.csv
-echo {1..4096} | tr ' ' "\n" | xargs -I{} ./bin/par-merge 10 1000000 {} 8 >> data/problem_three.csv
+echo "n,bc,num_threads,mergesort_time,stl_sort_time,speedup" > data/problem_three.csv
+for ((i=1; i<=100; i+=2))
+do
+	./bin/par-merge 10 1000000 $i 8 >> data/problem_three.csv
+	echo base case of size $i
+done	
+for ((i=100; i<=500000; i+=100))
+do
+	./bin/par-merge 10 1000000 $i 8 >> data/problem_three.csv
+	echo base case of size $i
+done	
 
