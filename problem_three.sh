2664f3f75cc06ad21424152255668b5c1323e38c
Brae Troutman <troupb18@gemini.deac.wfu.edu>
2022-10-04 18:56:21 -0400

    generate data for problem three analysis

--- problem_three.sh
+++ problem_three.sh
@@ -1,4 +1,4 @@
-echo "n,bc,num_threads,mergesort_time,stl_sort_time,speedup" > data/problem_three.csv
+echo "n,bc,num_threads,mergesort_time,stl_sort_time,speedup,efficiency" > data/problem_three.csv
 for ((i=1; i<=100; i+=2))
 do
 	./bin/par-merge 10 1000000 $i 8 >> data/problem_three.csv
